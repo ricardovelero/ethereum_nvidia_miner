ab73c382fb6dae9cb678abf2a0bd9c2c67534e77
Ricardo Rodriguez <ricardovelero@gmail.com>
2017-12-21 09:38:19 +0100

    Discover number of GPUs to setup
    Finds number of installed GPUs and use it in the setup loop.

--- files/nvidia-overclock.sh
+++ files/nvidia-overclock.sh
@@ -15,22 +15,35 @@ fi
 
 export DISPLAY=:0
 
-# Graphics card 1 to 6
-for MY_DEVICE in {0..5}
+#Common commands
+NVD=nvidia-settings
+SMI="sudo nvidia-smi"
+
+#Declare variables
+typeset -i i GPUS
+
+# Determine the number of available GPU's
+GPUS=$(nvidia-smi -i 0 --query-gpu=count --format=csv,noheader,nounits)
+
+echo -e "Detected: $GPUS GPU's"
+echo ""
+
+# Loop to setup found gpus
+for ((MY_DEVICE=0;MY_DEVICE<=GPUS;++MY_DEVICE))
 do
-	# Check if card exists
-	if nvidia-smi -i $MY_DEVICE >> /dev/null 2>&1; then
-		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUPowerMizerMode=1"
-		# Fan speed
-		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUFanControlState=1"
-		nvidia-settings -a "[fan:$MY_DEVICE]/GPUTargetFanSpeed=$MY_FAN"
-		# Graphics clock
-		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUGraphicsClockOffset[3]=$MY_CLOCK"
-		# Memory clock
-		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUMemoryTransferRateOffset[3]=$MY_MEM"
+        # Check if card exists
+        if ${SMI} -i $MY_DEVICE >> /dev/null 2>&1; then
+                ${NVD} -a "[gpu:$MY_DEVICE]/GPUPowerMizerMode=1"
+                # Manual Fan speed
+                ${NVD} -a "[gpu:$MY_DEVICE]/GPUFanControlState=1"
+                ${NVD} -a "[fan:$MY_DEVICE]/GPUTargetFanSpeed=$MY_FAN"
+                # Graphics clock
+                ${NVD} -a "[gpu:$MY_DEVICE]/GPUGraphicsClockOffset[2]=$MY_CLOCK"
+                # Memory clock
+                ${NVD} -a "[gpu:$MY_DEVICE]/GPUMemoryTransferRateOffset[2]=$MY_MEM"
                 # Set watt/powerlimit. This is also set in miner.sh at autostart.
-                sudo nvidia-smi -i "$MY_DEVICE" -pl "$MY_WATT"
-	fi
+                ${SMI} -i "$MY_DEVICE" -pl "$MY_WATT"
+        fi
 done
 
 echo
